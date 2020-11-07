024724db2f1bd3704ad5bc224fba5f2b12c3acaa
PyatiyEtaj <pyatiyEtaj@gmail.com>
2020-11-07 12:50:36 +0400

    refac

--- FO3Dll/Initialization.cpp
+++ FO3Dll/Initialization.cpp
@@ -2,21 +2,21 @@
 #include "framework.h"
 #include "FO3Funcs.h"
 
-HookSetter* hsCrittersProcess;
-bool initializationDone = false;
+HookSetter* _hsCrittersProcess;
+bool _initializationDone = false;
 void __fastcall hook_CrittersProcess(int* this_, PASS_ARG) {
 	InitFo3Funcs();
-	initializationDone = true;
+	_initializationDone = true;
 	typedef void (__fastcall* orig)(int*, PASS_ARG);
-	orig ret = orig(hsCrittersProcess->OriginalOps);
+	orig ret = orig(_hsCrittersProcess->OriginalOps);
 	ret(this_, 0);
 }
 
 void Initializator::SetInitializator() noexcept
 {
-	isInited_ = &initializationDone;
-	hsCrittersProcess = CrtHookSetter((PBYTE)GET_ADR(FO3_CRITTERS_PROCESS), (DWORD)hook_CrittersProcess, 5);
-    SetHookSetter(hsCrittersProcess);
+	isInited_ = &_initializationDone;
+	_hsCrittersProcess = CrtHookSetter((PBYTE)GET_ADR(FO3_CRITTERS_PROCESS), (DWORD)hook_CrittersProcess, 5);
+    SetHookSetter(_hsCrittersProcess);
 }
 
 void Initializator::Await() const noexcept
@@ -24,5 +24,5 @@ void Initializator::Await() const noexcept
 	while (!IsInit()) {
 		Sleep(100);
 	}
-	UnsetHook(hsCrittersProcess);
+	UnsetHook(_hsCrittersProcess);
 }
